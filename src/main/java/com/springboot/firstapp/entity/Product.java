a2b8c4652ddb26279a379a12efbbb738b409852c
Alvaro <jlinaresmedalla@gmail.com>
2023-07-17 06:36:56 -0500

    Creando tabla y customizandola con hibernate

---
+++ src/main/java/com/springboot/firstapp/entity/Product.java
@@ -0,0 +1,105 @@
+package com.springboot.firstapp.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity //Crea la entidad en la base de datos
+@Table(
+        name = "products",
+        schema = "ecommerce",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "sku_unique", columnNames = "stock_keeping_unit")
+        }
+) //Indica el nombre de la tabla y el esquema de la base de datos y las restricciones de unicidad de la tabla (SKU)
+public class Product {
+
+    @Id //Indica que es la llave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Indica que es autoincrementable
+    private Long id;
+    @Column(name = "stock_keeping_unit", nullable = false)
+    private String sku;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private boolean active;
+    private String imageURL;
+    private LocalDateTime dataCreated;
+    private LocalDateTime lastUpdated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public LocalDateTime getDataCreated() {
+        return dataCreated;
+    }
+
+    public void setDataCreated(LocalDateTime dataCreated) {
+        this.dataCreated = dataCreated;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+
+}

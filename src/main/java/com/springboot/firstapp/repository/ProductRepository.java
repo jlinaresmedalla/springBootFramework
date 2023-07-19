9daeac697ef88d022a6a328e20042fc4535f3fbb
jlinaresmedalla <jlinaresmedalla@gmail.com>
2023-07-19 10:40:19 -0500

    inicio de proyecto apirest CRUD en mysql

--- src/main/java/com/springboot/firstapp/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.firstapp.repository;
-
-import com.springboot.firstapp.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
---
+++ src/main/java/com/springboot/firstapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.springboot.firstapp.repository;
+
+import com.springboot.firstapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
